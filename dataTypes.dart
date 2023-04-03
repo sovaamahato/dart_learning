189ce008e2e16f1afdebf5e4ad5f8bdd302170ae
sova <mahatoshobha143@gmail.com>
2023-04-03 22:48:00 +0545

    dart basics

---
+++ dataTypes.dart
@@ -0,0 +1,19 @@
+void main() {
+  //var
+  //int
+  //String
+  //dyanmic
+
+  var a=80;
+  print(a);
+
+  //cannot change variable "a" datatype  as its assign integer once 
+  //  a="sova";
+  // print(a);
+
+  dynamic b= 40;
+  print(b);
+ b="sova";
+  print(b);
+
+}
---
+++ stringsss.dart
@@ -0,0 +1,8 @@
+void main() {
+  var s1 ="its me \n sova";
+  print(s1);
+
+  //r is used to avoid \n 
+  var s =r"it's me \n sova";
+  print(s);
+}
