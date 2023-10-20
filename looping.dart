5a428ce69fac3a94f63a18bcd9444aaa2b21066b
sova <mahatoshobha143@gmail.com>
2023-10-20 23:16:32 +0545

    day 2

---
+++ looping.dart
@@ -0,0 +1,29 @@
+void main(){
+  var a=20;
+  //for loop
+//   for(int i=0;i<10;i++){
+//     print(i+1);
+//   }
+  
+  //table of 3
+//   for(int i=1;i<=10;i++){
+//     print("3 * $i = ${i*3}" );
+//   }
+  
+  //while loop
+//   int i=1;
+//  while(i<=10){
+//     print("4 * $i = ${i*4}" );
+//    i++;
+//  }
+  //Do while loop
+  int i=1;
+do{
+  print("8 * $i = ${i*8}" );
+  i++;
+}  while(i<=10);   
+  
+  
+  
+  
+}
