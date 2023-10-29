de8af85cd5d001a4801b6ce8445049c208c95fc6
sova <mahatoshobha143@gmail.com>
2023-10-29 21:26:12 +0545

    class & obj

--- class&obj.dart
+++ class&obj.dart
@@ -1,19 +1,45 @@
-void main(){
+// void main(){
 
   
-  var obj1 =new Animal();
-  obj1.speak();
+//   var obj1 =new Animal();
+//   obj1.speak();
   
   
   
   
   
-}
-class Animal{
- var name;
+// }
+// class Animal{
+//  var name;
   
- speak(){
-    print("animal speak");
+//  speak(){
+//     print("animal speak");
     
-  }
-}
+//   }
+// }
+
+//example 2
+// Defining class  
+ class Student {  
+   var stdName;  
+   var stdAge;  
+   var stdRoll_nu;  
+     
+   // defining class function  
+    showStdInfo() {  
+        print("Student Name is : ${stdName}");  
+        print("Student Age is : ${stdAge}");  
+        print("Student Roll Number is : ${stdRoll_nu}");  
+  
+               }  
+}  
+void main () {  
+  
+  // Creating object called std  
+  var std = new Student();  
+  std.stdName = "Peter";  
+  std.stdAge =24;  
+  std.stdRoll_nu = 90001;  
+// Accessing class Function  
+ std.showStdInfo();  
+}  
