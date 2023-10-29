b032dbfd3d2091f5da828659f30a62d6daa34d82
sova <mahatoshobha143@gmail.com>
2023-10-29 21:28:25 +0545

    constructor

--- class&obj.dart
+++ class&obj.dart
@@ -20,26 +20,41 @@
 
 //example 2
 // Defining class  
- class Student {  
-   var stdName;  
-   var stdAge;  
-   var stdRoll_nu;  
+//  class Student {  
+//    var stdName;  
+//    var stdAge;  
+//    var stdRoll_nu;  
      
-   // defining class function  
-    showStdInfo() {  
-        print("Student Name is : ${stdName}");  
-        print("Student Age is : ${stdAge}");  
-        print("Student Roll Number is : ${stdRoll_nu}");  
+//    // defining class function  
+//     showStdInfo() {  
+//         print("Student Name is : ${stdName}");  
+//         print("Student Age is : ${stdAge}");  
+//         print("Student Roll Number is : ${stdRoll_nu}");  
   
-               }  
+//                }  
+// }  
+// void main () {  
+  
+//   // Creating object called std  
+//   var std = new Student();  
+//   std.stdName = "Peter";  
+//   std.stdAge =24;  
+//   std.stdRoll_nu = 90001;  
+// // Accessing class Function  
+//  std.showStdInfo();  
+// }  
+
+
+//constructor in dart
+void main() {  
+      // Creating an object   
+      Student std = new Student("Jones",26);  
 }  
-void main () {  
+class Student{  
+     // Declaring a construstor   
+     Student(String str, int age){  
+          print("The name is: ${str}");  
+          print("The age is: ${age}");  
   
-  // Creating object called std  
-  var std = new Student();  
-  std.stdName = "Peter";  
-  std.stdAge =24;  
-  std.stdRoll_nu = 90001;  
-// Accessing class Function  
- std.showStdInfo();  
+             }  
 }  
